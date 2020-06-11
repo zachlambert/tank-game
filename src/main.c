ff33895489da097ed4f751a0f84084de80ae3adf
Zach Lambert <zachalambert@gmail.com>
2020-06-11 19:43:46 +0100

    Finalised graphics

--- src/main.c
+++ src/main.c
@@ -8,8 +8,8 @@ int main(int argc, char *argv[])
     // Initialise with { ... }
     // Omitted fields are initialised to zero
     App app_obj = {
-        .SCREEN_WIDTH = 1280,
-        .SCREEN_HEIGHT = 720
+        .SCREEN_WIDTH = 1600,
+        .SCREEN_HEIGHT = 900 
     };
     App* app = &app_obj;
 
